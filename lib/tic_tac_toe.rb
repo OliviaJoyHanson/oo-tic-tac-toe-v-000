9e85ccf33c5fd7d93d6042fcb514669b4f9adda9
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-06-03 01:44:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,7 @@ class TicTacToe
   end
 
   def winner
-    turn_count.odd? ? "X" : "O"
+    turn_count.odd? ? "X" : "O" if won?
     #if won? && @board.select{|i| i == "X"}.length > @board.select{|i| i == "O"}.length
     #  "X"
     #elsif won? && @board.select{|i| i = "O"}.length > @board.select{|i| i == "X"}.length
