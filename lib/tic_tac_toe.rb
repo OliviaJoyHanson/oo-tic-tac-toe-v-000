ed6cf8e700619ba805fb898260ea0093308228d2
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-06-03 01:58:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,11 +74,11 @@ class TicTacToe
   end
 
   def winner
-    if won? && @board[won?[0] == "X"]
+    if won? && @board[won?[0]] == "X"
       #won?.all?{|e| @board[e] == "X"}
       #@board.select{|i| i == "X"}.length > @board.select{|i| i == "O"}.length
       "X"
-    elsif won? && @board[won?[0] == "X"]
+    elsif won? && @board[won?[0]] == "O"
       #won?.all?{|e| @board[e] == "O"}
       #won? && @board.select{|i| i = "O"}.length > @board.select{|i| i == "X"}.length
       "O"
